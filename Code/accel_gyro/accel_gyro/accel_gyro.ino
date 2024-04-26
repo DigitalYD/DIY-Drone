4e020042a70ced8a8d0738c684184fab83e90610
DigitalYD <114110126+DigitalYD@users.noreply.github.com>
2024-04-26 12:12:20 -0400

    LSM6DSOX works
    -issue: the Accel/Gyro sensor works, but the issue is the library is formatted wrong
    - soln: pivoting to a LSM9DS1 Accel/Gyro sensor

--- Code/accel_gyro/accel_gyro/accel_gyro.ino
+++ Code/accel_gyro/accel_gyro/accel_gyro.ino
@@ -3,150 +3,13 @@
 
 #include <Adafruit_LSM6DSOX.h>
 
-// For SPI mode, we need a CS pin
-#define LSM_CS 10
-// For software-SPI mode we need SCK/MOSI/MISO pins
-#define LSM_SCK 13
-#define LSM_MISO 12
-#define LSM_MOSI 11
-
 Adafruit_LSM6DSOX sox;
-void setup(void) {
-  Serial.begin(115200);
-  while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
 
-  Serial.println("Adafruit LSM6DSOX test!");
+float RateRoll, RatePitch, RateYaw;
+float RateCalibrationRoll, RateCalibrationPitch, RateCalibrationYaw;
+int RateCalibrationNumber;
 
-  if (!sox.begin_I2C()) {
-    // if (!sox.begin_SPI(LSM_CS)) {
-    // if (!sox.begin_SPI(LSM_CS, LSM_SCK, LSM_MISO, LSM_MOSI)) {
-    // Serial.println("Failed to find LSM6DSOX chip");
-    while (1) {
-      delay(10);
-    }
-  }
-
-  Serial.println("LSM6DSOX Found!");
-
-  // sox.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (sox.getAccelRange()) {
-  case LSM6DS_ACCEL_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case LSM6DS_ACCEL_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case LSM6DS_ACCEL_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case LSM6DS_ACCEL_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
-
-  // sox.setGyroRange(LSM6DS_GYRO_RANGE_250_DPS );
-  Serial.print("Gyro range set to: ");
-  switch (sox.getGyroRange()) {
-  case LSM6DS_GYRO_RANGE_125_DPS:
-    Serial.println("125 degrees/s");
-    break;
-  case LSM6DS_GYRO_RANGE_250_DPS:
-    Serial.println("250 degrees/s");
-    break;
-  case LSM6DS_GYRO_RANGE_500_DPS:
-    Serial.println("500 degrees/s");
-    break;
-  case LSM6DS_GYRO_RANGE_1000_DPS:
-    Serial.println("1000 degrees/s");
-    break;
-  case LSM6DS_GYRO_RANGE_2000_DPS:
-    Serial.println("2000 degrees/s");
-    break;
-  case ISM330DHCX_GYRO_RANGE_4000_DPS:
-    break; // unsupported range for the DSOX
-  }
-
-  // sox.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
-  Serial.print("Accelerometer data rate set to: ");
-  switch (sox.getAccelDataRate()) {
-  case LSM6DS_RATE_SHUTDOWN:
-    Serial.println("0 Hz");
-    break;
-  case LSM6DS_RATE_12_5_HZ:
-    Serial.println("12.5 Hz");
-    break;
-  case LSM6DS_RATE_26_HZ:
-    Serial.println("26 Hz");
-    break;
-  case LSM6DS_RATE_52_HZ:
-    Serial.println("52 Hz");
-    break;
-  case LSM6DS_RATE_104_HZ:
-    Serial.println("104 Hz");
-    break;
-  case LSM6DS_RATE_208_HZ:
-    Serial.println("208 Hz");
-    break;
-  case LSM6DS_RATE_416_HZ:
-    Serial.println("416 Hz");
-    break;
-  case LSM6DS_RATE_833_HZ:
-    Serial.println("833 Hz");
-    break;
-  case LSM6DS_RATE_1_66K_HZ:
-    Serial.println("1.66 KHz");
-    break;
-  case LSM6DS_RATE_3_33K_HZ:
-    Serial.println("3.33 KHz");
-    break;
-  case LSM6DS_RATE_6_66K_HZ:
-    Serial.println("6.66 KHz");
-    break;
-  }
-
-  // sox.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
-  Serial.print("Gyro data rate set to: ");
-  switch (sox.getGyroDataRate()) {
-  case LSM6DS_RATE_SHUTDOWN:
-    Serial.println("0 Hz");
-    break;
-  case LSM6DS_RATE_12_5_HZ:
-    Serial.println("12.5 Hz");
-    break;
-  case LSM6DS_RATE_26_HZ:
-    Serial.println("26 Hz");
-    break;
-  case LSM6DS_RATE_52_HZ:
-    Serial.println("52 Hz");
-    break;
-  case LSM6DS_RATE_104_HZ:
-    Serial.println("104 Hz");
-    break;
-  case LSM6DS_RATE_208_HZ:
-    Serial.println("208 Hz");
-    break;
-  case LSM6DS_RATE_416_HZ:
-    Serial.println("416 Hz");
-    break;
-  case LSM6DS_RATE_833_HZ:
-    Serial.println("833 Hz");
-    break;
-  case LSM6DS_RATE_1_66K_HZ:
-    Serial.println("1.66 KHz");
-    break;
-  case LSM6DS_RATE_3_33K_HZ:
-    Serial.println("3.33 KHz");
-    break;
-  case LSM6DS_RATE_6_66K_HZ:
-    Serial.println("6.66 KHz");
-    break;
-  }
-}
-
-void loop() {
-
+void read_IMU(void){
   //  /* Get a new normalized sensor event */
   sensors_event_t accel;
   sensors_event_t gyro;
@@ -155,31 +18,198 @@ void loop() {
   // there is a error in the template where the gyro and accelerometer values are swapped
   sox.getEvent(&gyro, &accel, &temp);
 
-  Serial.print("\t\tTemperature ");
-  Serial.print(temp.temperature);
-  Serial.println(" deg C");
+  RateRoll = gyro.gyro.x;
+  RatePitch = gyro.gyro.y;
+  RateYaw = gyro.gyro.z;
+
+  // Serial.print("\t\tTemperature ");
+  // Serial.print(temp.temperature);
+  // Serial.println(" deg C");
 
   /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("\t\tAccel X: ");
-  Serial.print(accel.acceleration.x);
-  Serial.print(" \tY: ");
-  Serial.print(accel.acceleration.y);
-  Serial.print(" \tZ: ");
-  Serial.print(accel.acceleration.z);
-  Serial.println(" m/s^2 ");
+  // Serial.print("\t\tAccel X: ");
+  // Serial.print(accel.acceleration.x);
+  // Serial.print(" \tY: ");
+  // Serial.print(accel.acceleration.y);
+  // Serial.print(" \tZ: ");
+  // Serial.print(accel.acceleration.z);
+  // Serial.println(" m/s^2 ");
 
   /* Display the results (rotation is measured in rad/s) */
-  Serial.print("\t\tGyro X: ");
-  Serial.print(gyro.gyro.x);
-  Serial.print(" \tY: ");
-  Serial.print(gyro.gyro.y);
-  Serial.print(" \tZ: ");
-  Serial.print(gyro.gyro.z);
-  Serial.println(" radians/s ");
-  Serial.println();
+  // Serial.print("\t\tGyro X: ");
+  // Serial.print(gyro.gyro.x);
+  // Serial.print(" \tY: ");
+  // Serial.print(gyro.gyro.y);
+  // Serial.print(" \tZ: ");
+  // Serial.print(gyro.gyro.z);
+  // Serial.println(" radians/s ");
+  // Serial.println();
 
-  delay(100);
+  // delay(100);
+}
 
+
+void setup(void)
+{
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit LSM6DSOX test!");
+
+  if (!sox.begin_I2C())
+  {
+    while (1)
+    {
+      delay(10);
+    }
+  }
+
+  Serial.println("LSM6DSOX Found!");
+
+  for (RateCalibrationNumber = 0; RateCalibrationNumber < 2000; RateCalibrationNumber++)
+  {
+    read_IMU();
+    RateCalibrationRoll+=RateRoll;
+    RateCalibrationPitch+=RatePitch;
+    RateCalibrationYaw+=RateYaw;
+    delay(1);
+  }
+
+  RateCalibrationRoll/=2000;
+  RateCalibrationPitch/=2000;
+  RateCalibrationYaw/=2000;   
+
+    // sox.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
+    // Serial.print("Accelerometer range set to: ");
+    // switch (sox.getAccelRange()) {
+    // case LSM6DS_ACCEL_RANGE_2_G:
+    //   Serial.println("+-2G");
+    //   break;
+    // case LSM6DS_ACCEL_RANGE_4_G:
+    //   Serial.println("+-4G");
+    //   break;
+    // case LSM6DS_ACCEL_RANGE_8_G:
+    //   Serial.println("+-8G");
+    //   break;
+    // case LSM6DS_ACCEL_RANGE_16_G:
+    //   Serial.println("+-16G");
+    //   break;
+    // }
+
+    // sox.setGyroRange(LSM6DS_GYRO_RANGE_250_DPS );
+    // Serial.print("Gyro range set to: ");
+    // switch (sox.getGyroRange()) {
+    // case LSM6DS_GYRO_RANGE_125_DPS:
+    //   Serial.println("125 degrees/s");
+    //   break;
+    // case LSM6DS_GYRO_RANGE_250_DPS:
+    //   Serial.println("250 degrees/s");
+    //   break;
+    // case LSM6DS_GYRO_RANGE_500_DPS:
+    //   Serial.println("500 degrees/s");
+    //   break;
+    // case LSM6DS_GYRO_RANGE_1000_DPS:
+    //   Serial.println("1000 degrees/s");
+    //   break;
+    // case LSM6DS_GYRO_RANGE_2000_DPS:
+    //   Serial.println("2000 degrees/s");
+    //   break;
+    // case ISM330DHCX_GYRO_RANGE_4000_DPS:
+    //   break; // unsupported range for the DSOX
+    // }
+
+    // sox.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
+    // Serial.print("Accelerometer data rate set to: ");
+    // switch (sox.getAccelDataRate()) {
+    // case LSM6DS_RATE_SHUTDOWN:
+    //   Serial.println("0 Hz");
+    //   break;
+    // case LSM6DS_RATE_12_5_HZ:
+    //   Serial.println("12.5 Hz");
+    //   break;
+    // case LSM6DS_RATE_26_HZ:
+    //   Serial.println("26 Hz");
+    //   break;
+    // case LSM6DS_RATE_52_HZ:
+    //   Serial.println("52 Hz");
+    //   break;
+    // case LSM6DS_RATE_104_HZ:
+    //   Serial.println("104 Hz");
+    //   break;
+    // case LSM6DS_RATE_208_HZ:
+    //   Serial.println("208 Hz");
+    //   break;
+    // case LSM6DS_RATE_416_HZ:
+    //   Serial.println("416 Hz");
+    //   break;
+    // case LSM6DS_RATE_833_HZ:
+    //   Serial.println("833 Hz");
+    //   break;
+    // case LSM6DS_RATE_1_66K_HZ:
+    //   Serial.println("1.66 KHz");
+    //   break;
+    // case LSM6DS_RATE_3_33K_HZ:
+    //   Serial.println("3.33 KHz");
+    //   break;
+    // case LSM6DS_RATE_6_66K_HZ:
+    //   Serial.println("6.66 KHz");
+    //   break;
+    // }
+
+    // sox.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
+    // Serial.print("Gyro data rate set to: ");
+    // switch (sox.getGyroDataRate()) {
+    // case LSM6DS_RATE_SHUTDOWN:
+    //   Serial.println("0 Hz");
+    //   break;
+    // case LSM6DS_RATE_12_5_HZ:
+    //   Serial.println("12.5 Hz");
+    //   break;
+    // case LSM6DS_RATE_26_HZ:
+    //   Serial.println("26 Hz");
+    //   break;
+    // case LSM6DS_RATE_52_HZ:
+    //   Serial.println("52 Hz");
+    //   break;
+    // case LSM6DS_RATE_104_HZ:
+    //   Serial.println("104 Hz");
+    //   break;
+    // case LSM6DS_RATE_208_HZ:
+    //   Serial.println("208 Hz");
+    //   break;
+    // case LSM6DS_RATE_416_HZ:
+    //   Serial.println("416 Hz");
+    //   break;
+    // case LSM6DS_RATE_833_HZ:
+    //   Serial.println("833 Hz");
+    //   break;
+    // case LSM6DS_RATE_1_66K_HZ:
+    //   Serial.println("1.66 KHz");
+    //   break;
+    // case LSM6DS_RATE_3_33K_HZ:
+    //   Serial.println("3.33 KHz");
+    //   break;
+    // case LSM6DS_RATE_6_66K_HZ:
+    //   Serial.println("6.66 KHz");
+    //   break;
+  
+}
+
+void loop()
+{
+  read_IMU();
+  
+  RateRoll-=RateCalibrationRoll;
+  RatePitch-=RateCalibrationPitch;
+  RateYaw-=RateCalibrationYaw;
+  Serial.print("Roll rate [°/s]= ");
+  Serial.print(RateRoll); 
+  Serial.print(" Pitch Rate [°/s]= ");
+  Serial.print(RatePitch);
+  Serial.print(" Yaw Rate [°/s]= ");
+  Serial.println(RateYaw);
+  delay(50);
   //  // serial plotter friendly format
 
   //  Serial.print(temp.temperature);
@@ -197,14 +227,7 @@ void loop() {
   //  delayMicroseconds(10000);
 }
 
-
-
-
-
 // testing and getting it to work
-
-
-
 
 // #include "LSM6DSOXSensor.h"
 
--- Code/main/main/main.ino
+++ Code/main/main/main.ino
@@ -11,13 +11,41 @@
 Adafruit_MPL3115A2 baro;
 Adafruit_LSM6DSOX sox;
 
+// TX/RX
 PulsePositionInput ReceiverInput(RISING);
 float ReceiverValue[] = {0, 0, 0, 0, 0, 0, 0, 0};
 int ChannelNumber = 0;
-int timer;
 
-// Servo motor;
-float InputThrottle;
+// drone motors
+float motor1, motor2, motor3, motor4;
+
+// pid values
+float RatePitch, RateRoll, RateYaw;
+float CalibrationPitch, CalibrationRoll, CalibrationYaw;
+int RateCalibrationNumber;
+
+uint32_t timer;
+
+float DesiredPitch, DesiredRoll, DesiredYaw;
+float ErrorPitch, ErrorRoll, ErrorYaw;
+float Throttle, Pitch, Roll, Yaw;
+float PrevErrorPitch, PrevErrorRoll, PrevErrorYaw;
+float PrevITermPitch, PrevITermRoll, PrevITermYaw;
+float PIDReturn[] = {0, 0, 0};
+
+float PRateRoll = 0.6;
+float PRatePitch = PRateRoll;
+float PRateYaw = 2;
+float IRateRoll = 3.5;
+float IRatePitch = IRateRoll;
+float IRateYaw = 12;
+float DRateRoll = 0.03;
+float DRatePitch = DRateRoll;
+float DRateYaw = 0;
+
+// imu
+float AccX, AccY, AccZ;
+float AngleRoll, AnglePitch;
 
 void read_receiver(void)
 {
@@ -35,12 +63,12 @@ void read_barometer(void)
 {
   float altitude = baro.getAltitude();
 
-  Serial.println("-----------------");
-  Serial.print("altitude = ");
-  Serial.print(altitude);
-  Serial.println(" m");
+  // Serial.println("-----------------");
+  // Serial.print("altitude = ");
+  // Serial.print(altitude);
+  // Serial.println(" m");
 
-  // delay(250);
+  // // delay(250);
 }
 
 void read_imu(void)
@@ -53,30 +81,28 @@ void read_imu(void)
   // there is a error in the template where the gyro and accelerometer values are swapped
   sox.getEvent(&gyro, &accel, &temp);
 
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("\t\tAccel X: ");
-  Serial.print(accel.acceleration.x);
-  Serial.print(" \tY: ");
-  Serial.print(accel.acceleration.y);
-  Serial.print(" \tZ: ");
-  Serial.print(accel.acceleration.z);
-  Serial.println(" m/s^2 ");
+  int16_t GyroX=gyro.gyro.x;
+  int16_t GyroY=gyro.gyro.y;
+  int16_t GyroZ=gyro.gyro.z;
+  RateRoll=(float)GyroX;
+  RatePitch=(float)GyroY;
+  RateYaw=(float)GyroZ;
+  AccX=(float)accel.acceleration.x;
+  AccY=(float)accel.acceleration.x;
+  AccZ=(float)accel.acceleration.x;
+  AngleRoll=atan(AccY/sqrt(AccX*AccX+AccZ*AccZ))*1/(3.142/180);
+  AnglePitch=-atan(AccX/sqrt(AccY*AccY+AccZ*AccZ))*1/(3.142/180);
 
-  /* Display the results (rotation is measured in rad/s) */
-  Serial.print("\t\tGyro X: ");
-  Serial.print(gyro.gyro.x);
-  Serial.print(" \tY: ");
-  Serial.print(gyro.gyro.y);
-  Serial.print(" \tZ: ");
-  Serial.print(gyro.gyro.z);
-  Serial.println(" radians/s ");
-  Serial.println();
-
-  // delay(100);
+  Serial.print("Acceleration X [g]= ");
+  Serial.print(AccX);
+  Serial.print(" Acceleration Y [g]= ");
+  Serial.print(AccY);
+  Serial.print(" Acceleration Z [g]= ");
+  Serial.println(AccZ);
 }
 
 // setting the motor control TX -> Teensy -> Motor
-void controller()
+void controller(void)
 {
   read_receiver();
   Serial.print("Number of channels: ");
@@ -91,9 +117,37 @@ void controller()
   Serial.println(ReceiverValue[3]);
 }
 
+void pid_controller(float Error, float P, float I, float D, float PrevError, float PrevIterm)
+{
+  float Pterm = P * Error;
+  float Iterm = PrevIterm + I * (Error + PrevError) * 0.004 / 2;
+  if (Iterm > 400)
+    Iterm = 400;
+  else if (Iterm < -400)
+    Iterm = -400;
+  float Dterm = D * (Error - PrevError) / 0.004;
+  float PIDOutput = Pterm + Iterm + Dterm;
+  if (PIDOutput > 400)
+    PIDOutput = 400;
+  else if (PIDOutput < -400)
+    PIDOutput = -400;
+  PIDReturn[0] = PIDOutput;
+  PIDReturn[1] = Error;
+  PIDReturn[2] = Iterm;
+}
+
+void reset_pid(void)
+{
+  PrevErrorRoll = 0;
+  PrevErrorPitch = 0;
+  PrevErrorYaw = 0;
+  PrevITermRoll = 0;
+  PrevITermPitch = 0;
+  PrevITermYaw = 0;
+}
+
 void setup()
 {
-
   // ************************************************************
   // Start Up Section
   // ************************************************************
@@ -107,14 +161,17 @@ void setup()
   // ************************************************************
   // IMU Section
   // ************************************************************
-  while (!Serial){
+  while (!Serial)
+  {
     Serial.println("fail IMU");
   }
-  
+
   Serial.println("Adafruit LSM6DSOX test!");
 
-  if (!sox.begin_I2C()) {
-    while (1) {
+  if (!sox.begin_I2C())
+  {
+    while (1)
+    {
       Serial.println("Could not find IMU. Check wiring.");
       delay(10);
     }
@@ -125,16 +182,18 @@ void setup()
   // ************************************************************
   // Barometer Section
   // ************************************************************
-  while (!Serial){
+  while (!Serial)
+  {
     Serial.println("fail Barometer");
   }
 
   Serial.println("Adafruit_MPL3115A2 test!");
-  
+
   if (!baro.begin())
   {
     Serial.println("Could not find Barometer. Check wiring.");
-    while (1);
+    while (1)
+      ;
   }
 
   Serial.println("Barometer Found!");
@@ -154,7 +213,13 @@ void setup()
 
   // analogWriteFrequency(PIN, FREQ)
   // set the PIN to send out and the frequency of the signal
+  CalibrationRoll /= 2000;
+  CalibrationPitch /= 2000;
+  CalibrationYaw /= 2000;
   analogWriteFrequency(1, 250);
+  analogWriteFrequency(2, 250);
+  analogWriteFrequency(3, 250);
+  analogWriteFrequency(4, 250);
 
   // analogWriteResolution(bits)
   analogWriteResolution(12);
@@ -166,7 +231,7 @@ void setup()
   while (ReceiverValue[2] < 1020 || ReceiverValue[2] > 1050)
   {
     read_receiver();
-    // delay(2);
+    delay(4);
   }
 }
 
@@ -174,10 +239,83 @@ void loop()
 {
   // the barometer has delay issues
   // read_barometer();
-  
-  read_imu();
 
-  InputThrottle = ReceiverValue[2];
-  analogWrite(1, 1.024 * InputThrottle);
+  RateRoll -= CalibrationRoll;
+  RatePitch -= CalibrationPitch;
+  RateYaw -= CalibrationYaw;
+
+  read_imu();
   controller();
+
+  DesiredRoll = 0.15 * (ReceiverValue[0] - 1500);
+  DesiredPitch = 0.15 * (ReceiverValue[1] - 1500);
+  Throttle = ReceiverValue[2];
+  DesiredYaw = 0.15 * (ReceiverValue[3] - 1500);
+
+  ErrorRoll = DesiredRoll - RateRoll;
+  ErrorPitch = DesiredPitch - RatePitch;
+  ErrorYaw = DesiredYaw - RateYaw;
+
+  pid_controller(ErrorRoll, PRateRoll, IRateRoll, DRateRoll, PrevErrorRoll, PrevITermRoll);
+  Roll = PIDReturn[0];
+  PrevErrorRoll = PIDReturn[1];
+  PrevITermRoll = PIDReturn[2];
+  pid_controller(ErrorPitch, PRatePitch, IRatePitch, DRatePitch, PrevITermPitch, PrevITermPitch);
+  Pitch = PIDReturn[0];
+  PrevErrorPitch = PIDReturn[1];
+  PrevITermPitch = PIDReturn[2];
+  pid_controller(ErrorYaw, PRateYaw, IRateYaw, DRateYaw, PrevErrorYaw, PrevITermYaw);
+  Yaw = PIDReturn[0];
+  PrevErrorYaw = PIDReturn[1];
+  PrevITermYaw = PIDReturn[2];
+
+  if (Throttle > 1800){
+    Throttle = 1800;
+  }
+
+  motor1 = 1.024 * (Throttle - Roll - Pitch - Yaw);
+  motor2 = 1.024 * (Throttle - Roll + Pitch + Yaw);
+  motor3 = 1.024 * (Throttle + Roll + Pitch - Yaw);
+  motor4 = 1.024 * (Throttle + Roll - Pitch + Yaw);
+  
+  if (motor1 > 1300)
+    motor1 = 1100;
+
+  if (motor2 > 1300)
+    motor2 = 1100;
+
+  if (motor3 > 1300)
+    motor3 = 1100;
+
+  if (motor4 > 1300)
+    motor4 = 1100;
+
+  int ThrottleIdle = 1180;
+
+  if (motor1 < ThrottleIdle)
+    motor1 = ThrottleIdle;
+
+  if (motor2 < ThrottleIdle)
+    motor2 = ThrottleIdle;
+
+  if (motor3 < ThrottleIdle)
+    motor3 = ThrottleIdle;
+
+  if (motor4 < ThrottleIdle)
+    motor4 = ThrottleIdle;
+
+  int ThrottleCutOff = 1000;
+
+  if (ReceiverValue[2] < 1050)
+  {
+    motor1 = ThrottleCutOff;
+    motor2 = ThrottleCutOff;
+    motor3 = ThrottleCutOff;
+    motor4 = ThrottleCutOff;
+    reset_pid();
+  }
+  analogWrite(1, motor1);
+  analogWrite(2, motor2);
+  analogWrite(3, motor3);
+  analogWrite(4, motor4);
 }
