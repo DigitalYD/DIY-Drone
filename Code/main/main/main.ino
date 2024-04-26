60db07ab96d9b4ad1dffc141a40472f5b2474fbe
DigitalYD <114110126+DigitalYD@users.noreply.github.com>
2024-04-25 23:01:40 -0400

    all three components work as expected but the brometer has a delay of 2-5 seconds

--- Code/main/main/main.ino
+++ Code/main/main/main.ino
@@ -1,8 +1,7 @@
 /* NOTIC: all of code for the receiver to connect to the teensy
-  * from Carbon Aeronautics
-  * https://github.com/CarbonAeronautics 
-  */
-
+ * from Carbon Aeronautics
+ * https://github.com/CarbonAeronautics
+ */
 
 #include <PulsePosition.h>
 #include <Wire.h>
@@ -13,23 +12,27 @@ Adafruit_MPL3115A2 baro;
 Adafruit_LSM6DSOX sox;
 
 PulsePositionInput ReceiverInput(RISING);
-float ReceiverValue[]={0, 0, 0, 0, 0, 0, 0, 0};
-int ChannelNumber=0; 
+float ReceiverValue[] = {0, 0, 0, 0, 0, 0, 0, 0};
+int ChannelNumber = 0;
 int timer;
 
 // Servo motor;
 float InputThrottle;
 
-void read_receiver(void){
+void read_receiver(void)
+{
   ChannelNumber = ReceiverInput.available();
-  if (ChannelNumber > 0) {
-      for (int i=1; i<=ChannelNumber;i++){
-    ReceiverValue[i-1]=ReceiverInput.read(i);
+  if (ChannelNumber > 0)
+  {
+    for (int i = 1; i <= ChannelNumber; i++)
+    {
+      ReceiverValue[i - 1] = ReceiverInput.read(i);
     }
   }
 }
 
-void read_barometer(void){
+void read_barometer(void)
+{
   float altitude = baro.getAltitude();
 
   Serial.println("-----------------");
@@ -40,58 +43,114 @@ void read_barometer(void){
   // delay(250);
 }
 
+void read_imu(void)
+{
+  //  /* Get a new normalized sensor event */
+  sensors_event_t accel;
+  sensors_event_t gyro;
+  sensors_event_t temp;
+
+  // there is a error in the template where the gyro and accelerometer values are swapped
+  sox.getEvent(&gyro, &accel, &temp);
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tAccel X: ");
+  Serial.print(accel.acceleration.x);
+  Serial.print(" \tY: ");
+  Serial.print(accel.acceleration.y);
+  Serial.print(" \tZ: ");
+  Serial.print(accel.acceleration.z);
+  Serial.println(" m/s^2 ");
+
+  /* Display the results (rotation is measured in rad/s) */
+  Serial.print("\t\tGyro X: ");
+  Serial.print(gyro.gyro.x);
+  Serial.print(" \tY: ");
+  Serial.print(gyro.gyro.y);
+  Serial.print(" \tZ: ");
+  Serial.print(gyro.gyro.z);
+  Serial.println(" radians/s ");
+  Serial.println();
+
+  // delay(100);
+}
 
 // setting the motor control TX -> Teensy -> Motor
-void controller() {
+void controller()
+{
   read_receiver();
   Serial.print("Number of channels: ");
   Serial.print(ChannelNumber);
   Serial.print(" Roll [µs]: ");
   Serial.print(ReceiverValue[0]);
-  Serial.print(" Pitch [µs]: "); 
+  Serial.print(" Pitch [µs]: ");
   Serial.print(ReceiverValue[1]);
-  Serial.print(" Throttle [µs]: "); 
+  Serial.print(" Throttle [µs]: ");
   Serial.print(ReceiverValue[2]);
-  Serial.print(" Yaw [µs]: "); 
+  Serial.print(" Yaw [µs]: ");
   Serial.println(ReceiverValue[3]);
 }
 
-
-void setup() {
+void setup()
+{
 
   // ************************************************************
-  // Start Up Section 
+  // Start Up Section
   // ************************************************************
-  // setting the initizing PIN 13 on board LED
-  pinMode(13, OUTPUT); 
-  digitalWrite(13, HIGH);
-
-  // begin the baud rate at 57,600 
+  // begin the baud rate at 57,600
   Serial.begin(115200);
 
-  // ************************************************************
-  // Barometer Section 
-  // ************************************************************
-   while (!Serial);
-  Serial.println("Adafruit_MPL3115A2 test!");
+  // setting the initizing PIN 13 on board LED
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
 
-  if (!baro.begin()) {
-    Serial.println("Could not find sensor. Check wiring.");
+  // ************************************************************
+  // IMU Section
+  // ************************************************************
+  while (!Serial){
+    Serial.println("fail IMU");
+  }
+  
+  Serial.println("Adafruit LSM6DSOX test!");
+
+  if (!sox.begin_I2C()) {
+    while (1) {
+      Serial.println("Could not find IMU. Check wiring.");
+      delay(10);
+    }
+  }
+
+  Serial.println("IMU Found!");
+
+  // ************************************************************
+  // Barometer Section
+  // ************************************************************
+  while (!Serial){
+    Serial.println("fail Barometer");
+  }
+
+  Serial.println("Adafruit_MPL3115A2 test!");
+  
+  if (!baro.begin())
+  {
+    Serial.println("Could not find Barometer. Check wiring.");
     while (1);
   }
 
-  // use to set sea level pressure for current location
-  // this is needed for accurate altitude measurement
-  // STD SLP = 1013.26 hPa
+  Serial.println("Barometer Found!");
+
+  baro.setAltitudeOffset(baro.getAltitudeOffset());
+
+  // set sea level pressure = 1013.26 hPa
   baro.setSeaPressure(1013.26);
 
   // ************************************************************
-  // Physical Controller Section 
+  // Physical Controller Section
   // ************************************************************
   // start reading from the signal line of the reciever at PIN 15
   ReceiverInput.begin(15);
 
-  // send PWM to the motor 
+  // send PWM to the motor
 
   // analogWriteFrequency(PIN, FREQ)
   // set the PIN to send out and the frequency of the signal
@@ -100,32 +159,25 @@ void setup() {
   // analogWriteResolution(bits)
   analogWriteResolution(12);
 
-  // delay 
+  // delay
   delay(250);
 
   // reading the throttle of the remote
-  while (ReceiverValue[2] < 1020 || ReceiverValue[2] > 1050) {
+  while (ReceiverValue[2] < 1020 || ReceiverValue[2] > 1050)
+  {
     read_receiver();
     // delay(2);
   }
-
 }
 
-void loop() {
+void loop()
+{
+  // the barometer has delay issues
+  // read_barometer();
   
-  
-  
-  // // read_receiver();
-  // Serial.print("barometer");
-  // Serial.println(timer);
-  read_barometer();
-  // delay(50);
+  read_imu();
 
-  InputThrottle=ReceiverValue[2];
-  analogWrite(1,1.024*InputThrottle);
-
-  // Serial.print("TX");
-  // Serial.println(timer);
+  InputThrottle = ReceiverValue[2];
+  analogWrite(1, 1.024 * InputThrottle);
   controller();
-  
 }
